ce41fa3ca517e0e56ee54511505dc5f7c2853983
Jay/Fienna Liang <roackb2@gmail.com>
2022-03-15 17:45:33 +0800

    Use Atropos to create mouse parallax effect

--- src/main.js
+++ src/main.js
@@ -1,1 +1,18 @@
-import '@/assets/styles/parallax.scss'
+import Atropos from 'atropos';
+import '@/assets/styles/index.scss'
+
+// Initialize
+const myAtropos = Atropos({
+  el: '.my-atropos',
+  activeOffset: 40,
+  shadowScale: 1.05,
+  onEnter() {
+    console.log('Enter');
+  },
+  onLeave() {
+    console.log('Leave');
+  },
+  onRotate(x, y) {
+    console.log('Rotate', x, y);
+  }
+});
